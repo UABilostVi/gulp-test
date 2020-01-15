a7bd1f28f6d7fcd830a301253a35fb28bafb2cd9
UABilostVi <bilostotskyi1991@gmail.com>
2020-01-15 12:02:14 +0200

    add gulpfile fron 01

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,52 @@
-const gulp = require('gulp'),
-      browserSync = require('browser-sync').create(),
-      scss = require('gulp-sass'),
-      autoprefixer = require('gulp-autoprefixer'),
-      rename = require('gulp-rename'),
-      cssbeautify = require("gulp-cssbeautify");
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
 
-gulp.task('compileCSS', function(){
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(scss().on('error', scss.logError))
-        .pipe(cssbeautify())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('app/css'))
+
+gulp.task('scss', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('default', function() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    });
-    gulp.watch('app/scss/**/*.scss').on("all", gulp.series('compileCSS', browserSync.reload));
-    gulp.watch('app/*.html').on("all", browserSync.reload);
+gulp.task('html', function(){
+  return gulp.src('app/*.html')
+  .pipe(browserSync.reload({stream: true}))
 });
+
+gulp.task('script', function(){
+  return gulp.src('app/js/*.js')
+  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function(){
+  return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "app/"
+      }
+  });
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('app/*.html', gulp.parallel('html'))
+  gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('default', gulp.parallel('scss', 'js', 'browser-sync', 'watch'))
